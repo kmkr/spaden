918b7047b80c29bcbb1b86a60865477eca3fb050
Travis CI <sveinung.rosaker@finn.no>
2016-09-07 11:17:00 +0000

    Deploy to GitHub Pages: 1547e08655387f79addc12d43ab972d1de4e2729

--- docs.js
+++ docs.js
@@ -1,31 +1,46 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// Menu button element
-var menu = document.getElementById('menu');
+// // Menu button element
+// var menu = document.getElementById('menu');
 
-// leftmenu element
-var leftmenu = document.getElementById('leftmenu');
+// // leftmenu element
+// var leftmenu = document.getElementById('leftmenu');
 
-// Nav element
-var nav = document.getElementsByTagName('nav')[0]
+// // Nav element
+// var nav = document.getElementsByTagName('nav')[0]
 
-// Toggle class function
-function toggleClass(element, className){
-    if (!element || !className){
-        return;
-    }
-    var classString = element.className, nameIndex = classString.indexOf(className);
-    if (nameIndex == -1) {
-        classString += ' ' + className;
-    }
-    else {
-        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
-    }
-    element.className = classString;
+// // Toggle class function
+// function toggleClass(element, className){
+//     if (!element || !className){
+//         return;
+//     }
+//     var classString = element.className, nameIndex = classString.indexOf(className);
+//     if (nameIndex == -1) {
+//         classString += ' ' + className;
+//     }
+//     else {
+//         classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
+//     }
+//     element.className = classString;
+// }
+
+// menu.onclick = function() {
+//     toggleClass(leftmenu, 'show');
+// };
+
+
+const treeMenu = document.getElementById('tree-menu');
+if (treeMenu) {
+    treeMenu.addEventListener('click', function (e) {
+        if (e.target) {
+            const ref = e.target.getAttribute('data-ref');
+            console.log('looking for ref', ref);
+            if (document.getElementById(ref)) {
+                document.location.hash = `#${ref}`;
+                e.preventDefault();
+                e.stopPropagation();
+            }
+        }
+    })
 }
-
-menu.onclick = function() {
-    toggleClass(leftmenu, 'show');
-};
-
 
 },{}]},{},[1]);
